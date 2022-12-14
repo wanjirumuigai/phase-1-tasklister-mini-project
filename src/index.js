0f917cf86c181ffa04c993c35bc2546010f94803
Pauline Muigai <wanjirumuigai@gmail.com>
2022-12-14 14:36:14 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("#create-task-form")
+  form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const todoList = document.getElementById("new-task-description")
+  createTodo(todoList.value)
+  form.reset()
+})
+
+function createTodo(todo) {
+
+  let p =document.createElement('li')
+  let btn = document.createElement('button')
+  let priority = document.querySelector('option')
+   
+  btn.addEventListener('click',handleDel)
+  p.textContent=`${todo} `
+  btn.textContent = 'x'
+  p.appendChild(btn)
+  p.appendChild(priority)
+  document.querySelector('#list').appendChild(p)
+}
+function handleDel(e) {
+  e.target.parentNode.remove()
+}
+
+
 });
+
+
+
